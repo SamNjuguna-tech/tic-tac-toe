f1cc6eaa6ef64d83e2fa7d7ee37a8d3f84d73281
SamNjuguna-tech <snjuguna898@gmail.com>
2022-05-10 20:35:22 +0300

    name

---
+++ script.js
@@ -0,0 +1,22 @@
+// let board =[
+//     ['O','X','O'],
+//     ['X','X','O'],
+//     ['X','O','X'],
+// ];
+
+// let player1= 'X';
+// let player2 = 'O';
+
+// function setup(){
+//     createCanvas(400, 400);
+// }
+// function draw() {
+//     background(220);
+//     for (let i = 0; i < 3; i++) {
+//         for (let j = 0; j < 3; j++) {
+            
+//         }
+        
+//     }
+// }
+// setup
